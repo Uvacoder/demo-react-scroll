0f1d81de1b5ebed22e11b2f8557b203137f20df9
Your Name <avneeshagarwal0612@gmail.com>
2021-09-12 18:58:58 +0530

    done with the tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div id="section1">
+        <h1>Section 1</h1>
+      </div>
+      <div id="section2">
+        <h1>Section 2</h1>
+      </div>
+      <div id="section3">
+        <h1>Section 3</h1>
+      </div>
+      <div id="section4">
+        <h1>Section 4</h1>
+      </div>
     </div>
   );
 }
